1e359e60dee4da2a8a18987c2f9e2a9342b64512
aeschalna <aeschalna@gmail.com>
2024-05-29 12:24:25 -0700

    clearing syntax errors

--- seeds.sql
+++ seeds.sql
@@ -4,7 +4,7 @@ VALUES (001, 'Egyptian Antiquties'),
     (003, 'Anthropology'),
     (004, 'Botany'),
     (005, 'Forensic Division'),
-    (006, 'Restorations')
+    (006, 'Restorations');
 
 INSERT INTO roles (role_id, title, salary, department_id)
 VALUES (1, 'Forensic Anthropologist', 96900.30, 005),
@@ -14,14 +14,14 @@ VALUES (1, 'Forensic Anthropologist', 96900.30, 005),
     (5, 'Computer Analysist', 90012.06, 006),
     (6, 'Ethnobotanist', 66100.66, 004),
     (7, 'Ethnographer', 75077.75, 003),
-    (8, 'Egyptologist', 90609.33, 001)
+    (8, 'Egyptologist', 90609.33, 001);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'Temperance', 'Brennan', 1, ''),
+VALUES (1, 'Temperance', 'Brennan', 1, NULL),
     (2, 'Jack', 'Hodgins', 2, '1'),
-    (3, 'Jane', 'Buikstra', 3, ''),
+    (3, 'Jane', 'Buikstra', 3, NULL),
     (4, 'Zack', 'Addy', 4, '1'),
     (5, 'Angela', 'Montenegro', 5, '1'),
-    (6, 'Julia', 'Kuicz', 6, ''),
-    (7, 'James', 'McWhorter', 7, ''),
-    (8, 'Camille', 'Saroyan', 8, '')
+    (6, 'Julia', 'Kuicz', 6, NULL),
+    (7, 'James', 'McWhorter', 7, NULL),
+    (8, 'Camille', 'Saroyan', 8, NULL);
